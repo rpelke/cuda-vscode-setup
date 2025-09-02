56c6839590dc6bd2496028dc7b5251b8621039ca
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2025-09-02 09:29:22 +0200

    Add CI pipeline for pybind test
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- examples/pybind/build.bash
+++ examples/pybind/build.bash
@@ -16,8 +16,14 @@ if [ -z "${CUDA_PATH}" ]; then
   echo "Error: CUDA_PATH is not set" >&2
   exit 1
 fi
+
+if [ -z "${CUDA_ARCHITECTURE}" ]; then
+  echo "Error: CUDA_ARCHITECTURE is not set" >&2
+  exit 1
+fi
+
 cmake \
-    -DCMAKE_CUDA_ARCHITECTURES=75 \
+    -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURE} \
     -DCUDA_PATH=${CUDA_PATH} \
     -DCMAKE_CUDA_COMPILER=${CUDA_PATH}/bin/nvcc \
     ..
