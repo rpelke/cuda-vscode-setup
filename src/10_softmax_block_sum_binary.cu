dcfb54ca0837077867600817caa18a7277065f84
ndegener <deg.niklas@gmail.com>
2025-10-17 11:30:28 +0200

    10 now in row-major

--- src/10_softmax_block_sum_binary.cu
+++ src/10_softmax_block_sum_binary.cu
@@ -56,44 +56,44 @@ __global__ void softmax_block_binary_k0(int M, int N, const float *A, float *C, 
 }
 
 // Kernel 1 does reduction across blocks (use a single y-block for this)
-__global__ void softmax_block_binary_k1(int M, int N, const float *A, float *C, float *temp, int gridDim_y_k0) {
+__global__ void softmax_block_binary_k1(int M, int N, const float *A, float *C, float *temp, int gridDim_x_k0) {
 
     // load temp into shared mem (split along x-axis)
-    extern __shared__ float temp_s[]; // BLOCKSIZE_10*gridDim_y_k0
+    extern __shared__ float temp_s[]; // BLOCKSIZE_10*gridDim_x_k0
 
     // Threads that exceed block should idle
-    if(2*threadIdx.y >= gridDim_y_k0) return;
+    if(2*threadIdx.x >= gridDim_x_k0) return;
 
     // Each thread loads it's start element and it's neighbour (if exists)
-    int globalStartElem = (blockIdx.x * blockDim.x + threadIdx.x) * gridDim_y_k0 + 2*threadIdx.y;
-    int localStartElem = threadIdx.x * gridDim_y_k0 + 2*threadIdx.y;
+    int globalStartElem = (blockIdx.y * blockDim.y + threadIdx.y) * gridDim_x_k0 + 2*threadIdx.x;
+    int localStartElem = threadIdx.y * gridDim_x_k0 + 2*threadIdx.x;
 
     temp_s[localStartElem] = temp[globalStartElem];
-    if(threadIdx.y+1 < gridDim_y_k0)
+    if(threadIdx.x+1 < gridDim_x_k0)
         temp_s[localStartElem + 1] = temp[globalStartElem+1];
 
     __syncthreads();
 
     // Each thread calculates a partial sum
     int i = 1;
-    for (; i <= gridDim_y_k0; i*=2) {
-        if(threadIdx.y % i == 0 && 2*threadIdx.y+i < gridDim_y_k0) {
+    for (; i <= gridDim_x_k0; i*=2) {
+        if(threadIdx.x % i == 0 && 2*threadIdx.x+i < gridDim_x_k0) {
             temp_s[localStartElem] += temp_s[localStartElem + i];
         }
         __syncthreads();
     }
 
     // Write result back to temp
-    if(threadIdx.y == 0)
+    if(threadIdx.x == 0)
         temp[globalStartElem] = temp_s[localStartElem];
 }
 
 // Kernel 2 does final calculation on reduced values
-__global__ void softmax_block_binary_k2(int M, int N, const float *A, float *C, float *temp, int gridDim_y_k0) {
+__global__ void softmax_block_binary_k2(int M, int N, const float *A, float *C, float *temp, int gridDim_x_k0) {
 
     const unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;
     const unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;
 
     // Only 1 op on data, GMEM access should be fine
-    C[x * N + y] = expf(A[x * N + y]) / temp[x*gridDim_y_k0];
+    C[y * N + x] = expf(A[y * N + x]) / temp[y*gridDim_x_k0];
 }
